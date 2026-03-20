45991a070ad3a709789faa2fa65417dc0d76d7df
moemoli <moli@imoli.moe>
2026-03-20 22:38:34 +0800

    debug

--- platform/base.py
+++ platform/base.py
@@ -9,23 +9,23 @@ class PlatformBase:
     def __init__(self, plugin):
         self.plugin = plugin
 
-    def is_join_enable(self, group_id: str) -> bool:
+    async def is_join_enable(self, group_id: str) -> bool:
         """检查入群批准是否开启"""
-        return self.plugin.get_kv_data(f"{group_id}_join_enable", False) or False
+        return await self.plugin.get_kv_data(f"{group_id}_join_enable", False) or False
 
-    def is_github_enable(self, group_id: str) -> bool:
+    async def is_github_enable(self, group_id: str) -> bool:
         """检查 GitHub 入群是否开启"""
-        return self.get_github_repo(group_id) is not None
+        return await self.get_github_repo(group_id) is not None
 
-    def get_github_repo(self, group_id: str) -> str | None:
+    async def get_github_repo(self, group_id: str) -> str | None:
         """获取 GitHub 仓库地址"""
-        return self.plugin.get_kv_data(f"{group_id}_join_github", None)
+        return await self.plugin.get_kv_data(f"{group_id}_join_github", None)
 
-    def get_github_repo_lastest_hash(self, group_id: str) -> str | None:
+    async def get_github_repo_lastest_hash(self, group_id: str) -> str | None:
         """获取 GitHub 仓库最新提交哈希"""
         github_token = self.plugin.config.get("github_token")
         github_repo = (
-            str(self.get_github_repo(group_id) or "")
+            str(await self.get_github_repo(group_id) or "")
             .replace("https://github.com/", "")
             .replace(".git", "")
         )
@@ -34,65 +34,65 @@ class PlatformBase:
             headers = {"Authorization": f"token {github_token}"}
         logger.info(f"Fetching latest commit hash for GitHub repo {github_repo} with headers: {headers}")
         github_api = f"https://api.github.com/repos/{github_repo}/commits?per_page=1"
-        result = fetch_url(github_api, headers=headers)
+        result = await fetch_url(github_api, headers=headers)
         if isinstance(result, dict):
             return result[0]["sha"] or None
         return None
 
-    def get_join_comment_blacklist(self, group_id: str) -> list[str]:
+    async def get_join_comment_blacklist(self, group_id: str) -> list[str]:
         """获取入群申请评论黑名单列表"""
         return str(
-            self.plugin.get_kv_data(f"{group_id}_join_comment_black", "") or ""
+            await self.plugin.get_kv_data(f"{group_id}_join_comment_black", "") or ""
         ).split(",")
         
-    def get_join_level(self, group_id: str) -> int:
+    async def get_join_level(self, group_id: str) -> int:
         """获取入群批准难度等级"""
-        return self.plugin.get_kv_data(f"{group_id}_join_level", 0) or 0
+        return await self.plugin.get_kv_data(f"{group_id}_join_level", 0) or 0
     
-    def can_reject(self, group_id: str) -> bool:
+    async def can_reject(self, group_id: str) -> bool:
         """获取是否自动拒绝"""
-        return self.plugin.get_kv_data(f"{group_id}_join_reject", False) or False
+        return await self.plugin.get_kv_data(f"{group_id}_join_reject", False) or False
     
-    def get_group_blacklist(self, group_id: str) -> list[str]:
+    async def get_group_blacklist(self, group_id: str) -> list[str]:
         """获取群黑名单列表"""
         return str(
-            self.plugin.get_kv_data(f"{group_id}_blacklist", "") or ""
+            await self.plugin.get_kv_data(f"{group_id}_blacklist", "") or ""
         ).split(",")
         
-    def get_global_blacklist(self, group_id: str) -> list[str]:
+    async def get_global_blacklist(self, group_id: str) -> list[str]:
         """获取全局黑名单列表"""
         return str(
-            self.plugin.get_kv_data(f"global_blacklist", "") or ""
+            await self.plugin.get_kv_data(f"global_blacklist", "") or ""
         ).split(",")
         
     async def can_approve(
         self, comment: str, group_id: str, level: int, user_id: str
     ):
         # 全局黑名单检测
-        for black in self.get_global_blacklist(group_id):
+        for black in await self.get_global_blacklist(group_id):
             if black.startswith(f"{user_id}"):
                 return False, "用户在全局黑名单中"
         # 群黑名单检测
-        for black in self.get_group_blacklist(group_id):
+        for black in await self.get_group_blacklist(group_id):
             if black.startswith(f"{user_id}"):
                 return False, "用户在黑名单中"
         # GitHub 入群检测
-        if self.is_github_enable(group_id):
+        if await self.is_github_enable(group_id):
             logger.info(f"GitHub join check enabled for group {group_id}, checking comment: {comment}")
-            lastest_hash = self.get_github_repo_lastest_hash(group_id)
+            lastest_hash =  await self.get_github_repo_lastest_hash(group_id)
             if (
                not (lastest_hash is not None
                 and comment != ""
                 and lastest_hash.startswith(comment)
-                and level >= self.get_join_level(group_id))
+                and level >= await self.get_join_level(group_id))
             ):
                 logger.info(f"GitHub join check failed for user {user_id} in group {group_id}: lastest_hash={lastest_hash}, comment={comment}, level={level}, required_level={self.get_join_level(group_id)}")
                 return False, "错误的 GitHub 提交哈希，或者入群等级不足"
         # 黑名单备注检测
-        for black in self.get_join_comment_blacklist(group_id):
+        for black in await self.get_join_comment_blacklist(group_id):
             if black in comment:
                 return False, "备注内容在黑名单中"
-        if level < self.get_join_level(group_id):
+        if level < await self.get_join_level(group_id):
             return False, "入群等级不足"
         return True, "入群申请通过"
 
